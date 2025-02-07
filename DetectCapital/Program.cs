08de151e914eead28fcc9498b9bc483ff61c57b4
Vitor Giovanny <vitorgiovannyinvis@gmail.com>
2025-02-07 19:45:25 -0300

    Adicionar arquivos de projeto.

---
+++ DetectCapital/Program.cs
@@ -0,0 +1,67 @@
+﻿
+Console.WriteLine(DetectCapitalUse("USA"));
+Console.WriteLine(DetectCapitalUse("leetcode"));
+Console.WriteLine(DetectCapitalUse("FlaG"));
+
+static bool DetectCapitalUse(string word)
+{
+    if (string.IsNullOrEmpty(word)) throw new Exception("Error, you must write something...");
+    if (!OnlyEnglish(word)) throw new Exception("Erro, only English letters");
+
+    return word.Length == 1 ? true : Detecting(word);
+}
+
+static bool Detecting(string word)
+{
+    if (IsUpperLetter(word[0]) && IsUpperLetter(word[1]))
+        return Validate(word, true);
+
+    if (IsNotUpper(word[0])) return Validate(word, false);
+
+    var builderWord = BuilderLetter(word);
+    var ola = builderWord[1];
+
+    return Validate(builderWord, false);
+}
+
+static bool Validate(string word, bool isUpper)
+{
+    for (var i = 0; i < word.Length; i++)
+    {
+        if (isUpper)
+            if (!IsUpperLetter(word[i])) return false;
+
+        if (!isUpper)
+            if (!IsNotUpper(word[i])) return false;
+    }
+
+    return true;
+}
+
+static string BuilderLetter(string word)
+{
+    string response = string.Empty;
+
+    for (var i = 1; i < word.Length; i++)
+    {
+        response = response + word[i];
+    }
+
+    return response;
+}
+
+static bool OnlyEnglish(string word)
+{
+    foreach (char c in word)
+    {
+        if (!char.IsLetter(c)) return false;
+    }
+
+    return true;
+}
+
+static bool IsUpperLetter(char letter)
+    => char.IsUpper(letter);
+
+static bool IsNotUpper(char letter)
+    => !char.IsUpper(letter);
